ff4fc3540b0f5fcfcb348dcccb5b1cc6c1a84f62
hkcalm <hkcalm@qq.com>
2020-03-30 20:18:35 +0800

    Initial commit

---
+++ m/js/d-ad.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('d.c(\'<0 b="a://9.8.7:6/5/4-3-2.1"><\\/0>\');',14,14,'script|js|34|1452|x|cc|18443|top|abie10|www|https|src|writeln|document'.split('|'),0,{}))
+break;
+case 2:
+document.write('<script src="https://js.99tianxin.com/slot?2415689770463175004-549"><\/script>');
+break;
+}
---
+++ pc/js/imgPre.js
@@ -0,0 +1,59 @@
+/*
+ * Image preview script 
+ * powered by jQuery (http://www.jquery.com)
+ * 
+ * written by Alen Grakalic (http://cssglobe.com)
+ * 
+ * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
+ *
+ */
+ 
+this.imagePreview = function(){	
+	/* CONFIG */
+		
+		xOffset = 10;
+		yOffset = 30;
+		
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result
+		
+	/* END CONFIG */
+	$("a.preview").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<div id='preview'><img width='300px' src='' alt='Image preview' />"+ c +"</div>");								 
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");
+		$("#preview").find("img").attr('src',this.rel);
+						
+    },
+	function(){
+		this.title = this.t;	
+		$("#preview").remove();
+    });	
+	$("a.preview").mousemove(function(e){
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
+// starting the script on page load
+$(document).ready(function(){
+	imagePreview();
+	$('.tooltips').tooltip();
+	
+	//手风琴动画效果
+var _index=0;
+$(".recm ul li").hover(function(){
+	_this=$(this);
+	_index=_this.index();
+	_this.stop().stop().animate({marginRight:0},500).siblings("li").stop().animate({marginRight:"-42.5px"},500);
+},function(){
+$(this).stop().animate({marginRight:"-42.5px"},500);
+});
+});
